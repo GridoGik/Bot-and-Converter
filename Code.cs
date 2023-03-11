1543c5d9905f3fcad4a330d4828a53b2b28fa1bb
GridoGik <119078833+GridoGik@users.noreply.github.com>
2023-03-11 14:09:30 +0300

    Update Code.cs

--- Code.cs
+++ Code.cs
@@ -1,10 +1,12 @@
 using System;
   main void() {
-  printf('Hello, world!');
+  printf('Welcome!');
 // Values
   int z = 37;
   int c = 94;
   int ux = z + c;
 
   Console.WriteLine(ux/6.0);
+  char a = "$"
+  string w = 'Double click to choose'
 }
