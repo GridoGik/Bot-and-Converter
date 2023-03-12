7daa6896161347c1cca9b7333603415f64d8aa2f
GridoGik <119078833+GridoGik@users.noreply.github.com>
2023-03-12 09:00:26 +0300

    Delete Clipboard.py

--- Clipboard.py
+++
@@ -1,9 +0,0 @@
-def contain():
-  a = str(input('Send your message'))
-  b = str(input('Send your message'))
-  return a, b
-clip = str(input('What are you want to get?'))
-if clip == 'a':
-  print(a)
-elif clip == 'b':
-  print(b)
